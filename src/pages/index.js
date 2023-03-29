5032e7052618082ae823fc688004e87fd3ac1127
Nic <nic@Ravens-Air.attlocal.net>
2023-03-28 21:24:31 -0500

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Nic playing acoustic guitar with friends"
+        src="/Users/nic/nic-s-portfolio/gatsbynetlify/src/images/polaroid.jpg"
+      />
     </Layout>
   )
 }

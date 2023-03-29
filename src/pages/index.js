60cd31d7cf4aad8a553a94f83ee3a3e29cab1b4a
Nic <nic@Ravens-Air.attlocal.net>
2023-03-29 00:02:39 -0500

    Tutorial Part 4 WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout pageTitle="Nic Icaza | Portfolio">
+      <p>Nic Icaza</p>
       <StaticImage
         alt="Nic playing acoustic guitar with friends"
         src="https://lh3.googleusercontent.com/pw/AMWts8DhFsZJp5d3YiJmVX8DfKU7MAvkD3vb0ZBpkgu-_1CrEfEARam3CooJmW8yq-Rh24UHlXhCmZ8haQej8KKqQSCQ5ewNQWepywpUYlH_u2GESdBTCuzuXQHNE_1fbB2E6N7I6C2qKChyKJJJgtDdB8dKYQ=w1170-h1560-s-no?authuser=0"

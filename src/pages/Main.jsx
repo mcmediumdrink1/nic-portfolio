2e12b8af470350aaf5586ecedc6d0e963a4bcfcb
Nic <nic@Ravens-Air.attlocal.net>
2023-04-01 18:39:18 -0500

    updated video on work page - 6:39PM

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,7 +5,8 @@ import pacificOcean from '../assets/USN-embark-pacific-ocean-nic.mp4'
 function Main() {
     return (
         <div className='main'>
-            <video src={pacificOcean} autoPlay loop muted />
+            <video src={pacificOcean} autoPlay loop muted playsinline type="video/mp4"/>
+
         </div>
     )
 }
